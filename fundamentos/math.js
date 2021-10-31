ae8e23f44e7e94d3daf36161643a7374bf16e3e2
Denes Rafael <denesdonato@hotmail.com>
2021-10-31 10:53:38 -0300

    Math

---
+++ fundamentos/math.js
@@ -0,0 +1,6 @@
+let raio = 5.6;
+
+let area = Math.PI * Math.pow(raio, 2);
+
+console.log(area);
+console.log(typeof Math)
---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,11 @@
+console.log(7 / 0.0001); // Retorna Infinity
+
+console.log("10" / 2);
+
+// NaN - not a number
+console.log("String" * 2);
+
+console.log(0.1 + 0.7); // sem precisao
+
+// console.log(10. toString);
+console.log((10).toString());
