a9654c638d78e31ca103532d84e9ddcaae826209
Denes Rafael <denesdonato@hotmail.com>
2021-11-02 21:11:01 -0300

    Object

---
+++ fundamentos/objeto.js
@@ -0,0 +1,14 @@
+const produto1 = {};
+
+produto1.nome = 'Celular Mega';
+produto1.preco = 4998.90;
+produto1['Desconto adicional'] = 0.4; //evitar chaves com espaços
+
+console.log(produto1);
+
+const produto2 = {
+    nome: 'Camisa',
+    preco: 79.9
+}
+
+console.log(produto2);
