a39f841f0861860a0d75bfd776986e09a5a25ffe
denesdonato <denesdonato@hotmail.com>
2022-04-17 23:20:25 -0300

    Comparação String e Números

---
+++ funcao/comparacaoString.js
@@ -0,0 +1,17 @@
+const cadeia_de_strings = '1, 2, 3, 4, 5, 6, 7, 8, 9'
+console.log('1' > '2')
+//output : false
+console.log('1' > '1')
+//output : false
+console.log('2' > '1')
+//output : true
+console.log('4' > '2')
+//output : true
+
+
+console.log('2' > '12')
+//output : true
+console.log('5' > '42')
+//output : true
+console.log('3' > '29')
+//output : true
