676e1cb3a6acaee610b9f3cfdcff48be7bf88932
denesdonato <denesdonato@hotmail.com>
2022-04-21 17:08:27 -0300

    Introdução a OO

---
+++ objeto/oo.js
@@ -0,0 +1,20 @@
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+
+    }
+}
+
+objeto.processamento()
+
+// Princípios importantes
+// 1. Abstração 
+// 2. Encapsulamento
+// 3. Herança (prototype)
+// 4. Polimorfismo
