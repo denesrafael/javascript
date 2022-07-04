2a7b8e4936a0a83d12f72145574095483d4417aa
denesdonato <denesdonato@hotmail.com>
2022-07-03 21:41:58 -0300

    Notação literal

---
+++ objeto/notacaoLiteral.js
@@ -0,0 +1,34 @@
+const a = 1
+const b = 2
+const c = 3
+
+// Antes da versao ES6
+//ECMA SCRIPT 2015
+const obj1 = { a: a, b: b, c: c }
+
+// Notacao atual
+const obj2 = { a, b, c }
+
+console.log(obj1, obj2)
+
+const nomeAttr = 'nota'
+const valorAttr = 7.87
+
+const obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3);
+
+//ou
+const obj4 = { [nomeAttr]: valorAttr }
+console.log(obj4);
+
+const obj5 = {
+    funcao1: function () {
+        //...
+    },
+    funcao2() {
+        //...
+    }
+}
+
+console.log(obj5);
